764e0fad6933367a73de563920946ec05c5c1072
Arman.Security <84366545+ArmanSecurity@users.noreply.github.com>
2023-02-10 17:11:05 -0500

    Update IPTracker.py

--- IPTracker.py
+++ IPTracker.py
@@ -2,6 +2,7 @@ import socket
 import argparse
 import time
 import concurrent.futures
+import sys
 
 def check_dns(ip):
     try:
@@ -18,8 +19,9 @@ if __name__ == "__main__":
     parser.add_argument("-silent", "--silent", help="Silent mode", action="store_true")
     parser.add_argument("-stats", "--stats", help="Display stats during the running scan", action="store_true")
     parser.add_argument("-t", "--threads", help="Number of concurrent threads to use (default 100)", type=int, default=100)
+    parser.add_argument("-d", "--description", help="Display hostname of valid IP (disabled as default)", action="store_true")
     args = parser.parse_args()
-    
+
     ip_list = []
     if args.list:
         with open(args.list, "r") as f:
@@ -27,9 +29,10 @@ if __name__ == "__main__":
     elif args.ip:
         ip_list = [args.ip]
     else:
-        print("Please provide either a list of IPs or a single IP using the -l or -p switch")
-        exit(1)
-        
+        # Read IPs from standard input
+        for line in sys.stdin:
+            ip_list.append(line.strip())
+
     start_time = time.time()
     valid_ips = 0
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
@@ -41,12 +44,19 @@ if __name__ == "__main__":
                 valid_ips += 1
                 if args.output:
                     with open(args.output, "a") as f:
-                        f.write(f"{ip}\n")
+                        if args.description:
+                            print(f"{ip} is valid, hostname: {hostname}")
+                        if not args.description:
+                            print(f"{ip}")
                 if not args.silent:
-                    print(f"\n{ip} is valid, hostname: {hostname}")
+                    if args.description:
+                        print(f"{ip} is valid, hostname: {hostname}")
+                    if not args.description:
+                        print(f"{ip}")
             if args.stats:
                 elapsed_time = time.time() - start_time
                 progress = (ip_list.index(ip) + 1) / len(ip_list) * 100
                 stats = f"\rProgress: {progress:.2f}%, Elapsed time: {elapsed_time:.2f} seconds, Valid IPs found: {valid_ips}"
                 print(stats, end="")
-    print("\nScan completed.")
+
+
